9791a7fcc87000c28e1556a8e14d4ff81945295d
hilikliming <hilikliming@gmail.com>
2015-05-20 21:12:53 -0600

    Commented getTrainingSamples and formatACLsas

--- formatACLsas.m
+++ formatACLsas.m
@@ -4,19 +4,22 @@ function [ TSo ] = formatACLsas( AC, N_s )
 %  N_s - Desired frequency bin number per aspect observation
 %% Output:
 %  TS - Target Strength AC with N_s frequency bins
-%%%%%% AC = 20*log10(AC); no.
+
 [N,K] = size(AC);
-%TS = zeros(N_s,size(AC,2));
-eps = 3;
+eps   = 3;
+
 TS = AC(:,sum(AC,1)>eps);
+% Determining which side to shift to
 sub1 = AC(:,1:floor(K/2)); sub1 =sub1(:,sum(sub1,1)>eps);
 sub2 = AC(:,floor(K/2):K); sub2 =sub2(:,sum(sub2,1)>eps);
 bc = max([size(sub1,2),size(sub2,2)]);
+
+% Shifting to center TSo portion
 TS = circshift(TS,bc,2);
+
 TSo = zeros(N_s,size(TS,2));
 for k = 1:size(TS,2)
 TSo(:,k) = resample(TS(:,k),N_s,size(TS,1));
-%TSo(:,k) = 20*log10(abs(TSo(:,k))/norm(TSo(:,k)));
 end
 
 end
--- getTrainingSamples.m
+++ getTrainingSamples.m
@@ -4,22 +4,22 @@ trainList = dirMapDB;
 i = 1;
 Ytrain = struct([]);
 
-  for obj = 1:size(trainList,2)
-      DD=[];
-      for e = 1:size(trainList,1);
-        for tag = trainList(e,obj,:,:);
-            cd(char(tag));
-            x = what;
-            x = x.mat;
-            ob = open(char(x));
-            AC = ob.acp'; % Opening AC in directory
-            cd(home);
-            DD = [DD,formatACLsas(AC,301)]; % This script outputs target strength
-            i = i +1;
-        end
-      end
-      Ytrain(obj).D = DD;
-   end
+for obj = 1:size(trainList,2)
+  DD=[];
+  for e = 1:size(trainList,1);
+    for tag = trainList(e,obj,:,:);
+        cd(char(tag)); % navigate to the directory in dirMapDB=trainList
+        x = what;
+        x = x.mat; %grab the string of the .mat in directory
+        ob = open(char(x));
+        AC = ob.acp'; % Opening AC in directory
+        cd(home);
+        DD = [DD,formatACLsas(AC,301)]; % This script outputs target strength
+        i = i +1;
+    end
+  end
+  Ytrain(obj).D = DD;
+end
 
 
 end
